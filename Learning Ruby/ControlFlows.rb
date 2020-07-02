b61704f08283abafc585214429cfc056f05304b4
Julie Chung <j00leee@users.noreply.github.com>
2020-07-02 14:00:54 -0400

    learning control flows

---
+++ Learning Ruby/ControlFlows.rb
@@ -0,0 +1,27 @@
+#learning ruby through codecademy 
+
+#control flows
+#if else statements 
+if 2 < 3
+	print "true"
+elsif 
+	print "same"
+else 
+	print "false"
+end
+
+#unless statement. reverses if else statements
+hungry = false
+unless 
+	puts "i'm writing my ruby programs"
+else
+	puts "time to eat"
+end
+
+#operators 
+isTrue = 2 != 3
+isFalse = 2 == 3
+test1 = 17 > 16
+test2 = 21 < 30 
+test3 = 9 >= 9
+test4 = -10 < 3
