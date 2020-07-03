840ed01bdc564b44db33e6c23bbc9ae155d6524f
Julie Chung <j00leee@users.noreply.github.com>
2020-07-03 16:05:07 -0400

    completed control flow lesson

--- Learning Ruby/ControlFlows.rb
+++ Learning Ruby/ControlFlows.rb
@@ -55,3 +55,53 @@ boolean_2 = false
 
 #boolean_3 = !(700 / 10 == 70)
 boolean_3 = false
+
+#combining operators
+#boolean_1 = (3 < 4 || false) && (false || true)
+boolean_1 = true
+
+#boolean_2 = !true && (!true || 100 != 5**2)
+boolean_2 = false
+
+#boolean_3 = true || !(true || false)
+boolean_3 = true
+
+#if, else, elsif
+a = 5
+b = 4
+if a > b
+	print "a is greater than b"
+elsif b > a
+	print "b is greater than b"
+else 
+	print "b is equal to a"
+end 
+
+#unless 
+#codecademy example:
+problem = false
+print "Good to go" unless problem 
+
+#my example:
+number = false
+print "good" unless number
+
+#compare
+#test_1 should be false
+test_1 = 1 > 2
+
+#test_2 should be false
+test_2 = -2 > -1
+
+#test_3 should be true 
+test_3 = 3 > 2
+
+#booleans
+#test1 should be true 
+test1 = (1 > 0) && (1 == 1)
+
+#test2 should be true 
+test2 = !(false)
+
+#test3 should be false
+test3 = (1 == 7) || (0 > 4)
