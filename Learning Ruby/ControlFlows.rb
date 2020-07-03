e2935c8c54e6f1d7468dde968be836bf980d753e
Julie Chung <j00leee@users.noreply.github.com>
2020-07-02 22:30:49 -0400

    and, or, not operators

--- Learning Ruby/ControlFlows.rb
+++ Learning Ruby/ControlFlows.rb
@@ -25,3 +25,33 @@ test1 = 17 > 16
 test2 = 21 < 30 
 test3 = 9 >= 9
 test4 = -10 < 3
+
+#and
+#boolean_1 = 77 < 78 && 77 < 77
+boolean_1 = false
+
+#boolean_2 = true && 100 >= 100
+boolean_2 = true
+
+#boolean_3 = 2**3 == 8 && 3**2 == 9
+boolean_3 = true
+
+#or
+#boolean_1 = 2**3 != 3**2 || true
+boolean_1 = true
+
+#boolean_2 = false || -10 > -9
+boolean_2 = false
+
+#boolean_3 = false || false
+boolean_3 = false
+
+#not
+# boolean_1 = !true
+boolean_1 = false
+
+#boolean_2 = !true && !true
+boolean_2 = false
+
+#boolean_3 = !(700 / 10 == 70)
+boolean_3 = false
