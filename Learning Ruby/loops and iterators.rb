864062295310ed3bc8be937d37115e036a199b10
Julie Chung <j00leee@users.noreply.github.com>
2020-07-11 21:13:36 -0400

    finished loops and iterators lesson

--- Learning Ruby/loops and iterators.rb
+++ Learning Ruby/loops and iterators.rb
@@ -53,3 +53,63 @@ loop do
 	print "#{i}"
 	break if i <= 0
 end
+
+#loop to skip odd numbers 
+i = 20 
+loop do 
+	i -= 1
+	next if i % 2 == 1 #to skip even numbers: i % 2 == 0
+	print "#{i}"
+	break if i <= 0 
+end
+
+#array
+my_array = [1, 2, 3, 4, 5]
+
+#.each iterator example
+array = [1, 2, 3, 4, 5]
+array.each do |x|
+	x += 10 
+	print "#{x}"
+end
+
+#multiply each array element by 2
+odds = [1, 3, 5, 7, 9]
+odds.each do |i|
+	i *= 2
+	print "#{i}"
+end
+#another way: odds.each {|i| print "#{i *= 2}"}
+
+#.times iterator: super compact for loop. this will print out the sentence 5 times.
+5.times {print "wear a mask and be positive! "}
+
+#print 1 to 50 
+i = 1
+while i < 51 do 
+	print i 
+	i += 1 
+end
+
+#same as above except using until 
+i = 1 
+until i == 51 do 
+	print i 
+	i += 1 
+end
+
+#same as last two loops using for loop instead 
+for i in 1..50 
+	print i 
+end
+
+#print out Ruby! 30 times 
+i = 0 
+loop do 
+	i += 1 
+	print "Ruby!"
+	break if i == 30 
+end
+
+#same as above except using .times iterator
+30.times {print "Ruby!"}
